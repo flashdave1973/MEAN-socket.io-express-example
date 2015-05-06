62613dfd13cccc0ea9b1c5a5a09cbe970cda8075
flashdave1973 <flashdave1973@users.noreply.github.com>
2015-05-06 11:30:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 
 var express = require('express')
 ,	mongoose = require('mongoose')
-,	router = express.Router()
 ,	app = express()
 ,	port = process.env.PORT || 8080
 ,	io = require('socket.io').listen( app.listen(port) );
@@ -10,6 +9,6 @@ var express = require('express')
 mongoose.connect('mongodb://localhost/the-simple');
 
 require('./config')(app, io);
-require('./routes')(app, router, mongoose, io);
+require('./routes')(app, mongoose, io);
 
 console.log('Your application is running on http://localhost:' + port);
